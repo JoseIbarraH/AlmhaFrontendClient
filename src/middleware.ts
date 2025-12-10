b8a3342a577e6d76bd1379f0ea611dea3e799dc7
Jose Carlos Ibarra Herrera <jcibarrah1423@gmail.com>
2025-12-10 08:52:53 -0500

    import img

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,9 @@
-// Importa solo los tipos necesarios de Astro: APIContext
 import type { APIContext } from 'astro';
 
-// Importa tus utilidades y tipos
 import api from './lib/api';
 import type { DesignSettingsResponse } from "@/types/design"
 import type { ApiResponse } from "@/types/apiResponse"
 
-
-// Define la función `onRequest`.
-// Usamos el tipo estándar `Response` (del entorno de ejecución) para tipificar la función de retorno.
 export const onRequest = async (
     context: APIContext,
     next: () => Promise<Response>
