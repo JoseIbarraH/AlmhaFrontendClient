7f5a2fb6c1c8d7cc9eb80350e11fefd984bd1a2e
Jose Carlos Ibarra Herrera <jcibarrah1423@gmail.com>
2025-12-20 00:06:45 -0500

    api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const baseURL = import.meta.env.PUBLIC_API_URL;
 
+if (!baseURL) {
+  throw new Error("API_URL is not defined");
+}
+
 export function createApi(lang: string) {
   const api = axios.create({
     baseURL,
