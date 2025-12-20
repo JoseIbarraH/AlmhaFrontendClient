6df5430248bb7cabb881e4c0961e04ed2df45e08
Jose Carlos Ibarra Herrera <jcibarrah1423@gmail.com>
2025-12-20 00:01:17 -0500

    fix try api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.PUBLIC_API_URL || "http://localhost:8000";
+const baseURL = import.meta.env.PUBLIC_API_URL;
 
 export function createApi(lang: string) {
   const api = axios.create({
